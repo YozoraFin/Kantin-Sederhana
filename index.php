c28d1a95c1aac39df56d45e691d878b443d85b5f
YozoraFin <arjunamarcel25@gmail.com>
2026-05-30 10:01:42 +0700

    Init part 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El Kantin</title>
+</head>
+<body>
+    <h1>Ini web Kantin yaaa</h1>
+</body>
+</html>
